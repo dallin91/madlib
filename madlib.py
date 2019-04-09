b03f3ad09480cd7c347d925d00ed3a4313e8ae95
dallin91 <dallinreeves@gmail.com>
2019-04-08 19:40:05 -0500

    added file

---
+++ madlib.py
@@ -0,0 +1,13 @@
+# This is a basic madlib project
+# Created by Dallin Reeves
+# April 8, 2019 7:28 PM
+# The original haiku is by Alisha Mead
+# retrieved from https://www.familyfriendpoems.com/poems/other/haiku/
+
+noun = input('Give me a noun: ')
+bpart = input('Give me a body part: ')
+verb = input('Finally, give me a verb: ')
+
+print('I am over', noun.upper())
+print('Then my', bpart.upper(), 'meet yours once more,')
+print('and I', verb.upper(), 'in love...')
